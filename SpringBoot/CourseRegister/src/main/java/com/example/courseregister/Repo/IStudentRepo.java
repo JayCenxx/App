75c71b7fee1aef6c196811eb531fa69730d55fb4
JayCenxx <97939825+JayCenxx@users.noreply.github.com>
2022-09-25 04:23:17 -0400

    Delete SpringBoot/CourseRegister directory

--- SpringBoot/CourseRegister/src/main/java/com/example/courseregister/Repo/IStudentRepo.java
+++
@@ -1,5 +0,0 @@
-package com.example.courseregister.Repo;
-import com.example.courseregister.Entities.Student;
-import org.springframework.data.jpa.repository.JpaRepository;
-public interface IStudentRepo extends JpaRepository<Student,Integer> {
-}
--- SpringBoot/CourseRegister/src/main/java/com/example/courseregister/Repo/ITeacher.java
+++
@@ -1,7 +0,0 @@
-package com.example.courseregister.Repo;
-
-import com.example.courseregister.Entities.Teacher;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ITeacher extends JpaRepository<Teacher,Integer> {
-}
